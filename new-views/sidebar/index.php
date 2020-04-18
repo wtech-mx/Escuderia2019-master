9b3e5529d5c026cbddc4fa482ff6c5976532be1a
W-Tech-mx <52049354+GOLDBN@users.noreply.github.com>
2020-04-17 20:03:34 -0500

    new file view subastas

---
+++ new-views/sidebar/index.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Subastas-Eago</title>
+
+
+<?php
+
+    include("estilos.php");
+
+  ?>
+
+
+</head>
+
+<body>
+
+    <div class="wrapper">
+<?php 
+    include('navbar.php');
+ ?>
+
+        <!-- Page Content  -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #05123F">
+                <div class="container-fluid">
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-align-left"></i>
+                        <span>Menu</span>
+                    </button>
+
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link text-white" href="#">welcome: josue adrian ramirez</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="#">salir</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+                <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-md-12">
+                            <img class="img-fluid" src="http://eago.com.mx/q-subastas/images/banner970x90.png" alt="">
+
+                    </div>
+                </div>
+
+               
+                    <div class="col-md-12 p-3">
+                        <div class="row">
+                             <div class="col-auto">
+                                <img class="rounded p-2" src="icons/auction.png" alt="" style="background-color: #05123F;width: 50px;height: 50px">
+                            </div>
+
+                             <div class="col-auto">
+                                <h3>Subastas</h3>
+                                <p>Subasta abierta</p>
+                            </div>
+
+                            <div class="col-auto p-3">
+                                <button class="btn btn-info boton-redondeado">Activa</button>
+                            </div>
+
+                            
+                        </div>
+
+                    </div>
+                
+
+                <div class="row ">
+                    <div class="col-12 col-sm-6 col-md p-3">
+                            <div class="card text-center" style="height: 97%;">
+                                <h5 class="card-title p-4">Termina en:</h5>
+                              <div class="card-body align-self-center" style="padding: 30% 0 0 0">
+                                    <div class="">
+                                        <a href="#" class="btn btn-info ">
+                                        <div class="bg-info " style="width: 50px;height: 50px;font-size: 40px">
+                                            5
+                                        </div>
+                                         </a>
+                                         <p class="card-text">
+                                            <a href="" title="">05 : 41 : 29</a>
+                                        </p>  
+                                    </div>                              
+                                
+                              </div>
+                            </div>
+                    </div>
+
+                    <div class="col-12 col-sm-6 col-md p-3 ">
+                        <div class="card" >
+                          <div class="card-body">
+                            <h5 class="card-title mb-4">Activos</h5>
+                            <p class="card-text"></p>
+
+                              <label class="sr-only" for="">Ferrari-124spider</label>
+                              <div class="input-group mb-2 mr-sm-2">
+                                <input type="text" class="form-control" id="" placeholder="Ferrari-124spider" style="border-radius: 15px  0 0 15px;">
+                                <div class="input-group-prepend" >
+                                  <div class="bg-info input-derecho" >01:34:28</div>
+                                </div>
+                              </div>
+
+                              <label class="sr-only" for="">Ferrari-124spider</label>
+                              <div class="input-group mb-2 mr-sm-2">
+                                <input type="text" class="form-control" id="" placeholder="Ferrari-124spider" style="border-radius: 15px  0 0 15px;">
+                                <div class="input-group-prepend" >
+                                  <div class="bg-info input-derecho" >01:34:28</div>
+                                </div>
+                              </div>
+
+
+                              <label class="sr-only" for="">Ferrari-124spider</label>
+                              <div class="input-group mb-2 mr-sm-2 mb-4">
+                                <input type="text" class="form-control" id="" placeholder="Ferrari-124spider" style="border-radius: 15px  0 0 15px;">
+                                <div class="input-group-prepend" >
+                                  <div class="bg-info input-derecho" >01:34:28</div>
+                                </div>
+                              </div>
+
+
+                               <h5 class="card-title mb-4">Proximos</h5>
+
+                               <label class="sr-only" for="">Ferrari-124spider</label>
+                              <div class="input-group mb-2 mr-sm-2">
+                                <input type="text" class="form-control" id="" placeholder="Ferrari-124spider" style="border-radius: 15px  0 0 15px;">
+                                <div class="input-group-prepend" >
+                                  <div class="bg-danger input-derecho  text-white" >01:34:28</div>
+                                </div>
+                              </div>
+
+
+                              <label class="sr-only" for="">Ferrari-124spider</label>
+                              <div class="input-group mb-2 mr-sm-2">
+                                <input type="text" class="form-control" id="" placeholder="Ferrari-124spider" style="border-radius: 15px  0 0 15px;">
+                                <div class="input-group-prepend" >
+                                  <div class="bg-danger input-derecho text-white" >01:34:28</div>
+                                </div>
+                              </div>
+
+
+                              <label class="sr-only" for="">Ferrari-124spider</label>
+                              <div class="input-group mb-2 mr-sm-2">
+                                <input type="text" class="form-control input-izquerdo" id="" placeholder="Ferrari-124spider" >
+                                <div class="input-group-prepend" >
+                                  <div class="bg-danger input-derecho text-white" >01:34:28</div>
+                                </div>
+                              </div>
+
+
+                          </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-sm-6 col-md p-3">
+
+                      <div class="row mb-4">
+                            <div class="col-md-12">
+                              <div class="card" >
+                              <div class="card-body">
+                                <h5 class="card-title">Ofertas:</h5>   
+                                <div class="col-md-12 p-3">
+                                    <div class="row">
+                                         <div class="col-md-2 ">
+                                            <img class="border-circle p-2" src="icons/auction.png" alt="" style="background-color: #05123F;width: 50px;height: 50px;">
+                                        </div>
+
+                                        <div class="col-md-4 p-3">
+                                            <table class="">
+                                              <thead>
+                                                <tr>
+                                                  <th scope="col">Ofertantes</th>
+                                                  <th scope="col">Total Ofertas</th>
+     
+                                                </tr>
+                                              </thead>
+                                              <tbody>
+                                                <tr>
+                                                  <td>60</td>
+                                                  <td>88</td>
+
+                                                </tr>
+                                              </tbody>
+                                            </table>  
+                                        </div>
+                                    </div>
+
+                                </div>
+                              </div>
+                             </div>
+                            </div>
+                        </div>
+
+
+                      <div class="row mb-2">
+                            <div class="col-md-12">
+                              <div class="card" >
+                              <div class="card-body">
+                                <h5 class="card-title">Autos Subastas:</h5>   
+                                <div class="col-md-12 p-3">
+                                    <div class="row">
+                                         <div class="col-md-2">
+                                            <img class="border-circle p-2" src="icons/automotive.png" alt="" style="background-color: #05123F;width: 50px;height: 50px;">
+                                        </div>
+
+                                        <div class="col-md-4 p-3">
+                                            <table class="">
+                                              <thead>
+                                                <tr>
+                                                  <th scope="col">Autos</th>
+                                                  <th scope="col">Ofertados</th>
+                                                  <th scope="col">Vendidos</th>
+                                                </tr>
+                                              </thead>
+                                              <tbody>
+                                                <tr>
+                                                  <td>60</td>
+                                                  <td>88</td>
+                                                  <td>88</td>
+                                                </tr>
+                                              </tbody>
+                                            </table>  
+                                        </div>
+                                    </div>
+
+                                </div>
+                              </div>
+                             </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="overlay"></div>
+
+<?php 
+
+    include("scripts.php");
+
+ ?>
+
+</body>
+
+</html>
---
+++ new-views/sidebar/index2.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Subastas-Eago</title>
+
+
+<?php
+
+    include("estilos.php");
+
+  ?>
+
+
+</head>
+
+<body>
+
+    <div class="wrapper">
+<?php 
+    include('navbar.php');
+ ?>
+
+        <!-- Page Content  -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light"  style="background-color: #05123F">
+
+                <div class="container-fluid">
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-align-left"></i>
+                        <span>Menu</span>
+                    </button>
+
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link text-white" href="#">welcome: josue adrian ramirez</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="#">salir</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <div class="container-fluid">
+              <div class="row">
+                     <div class="col-md-12">
+                              <img class="img-fluid" src="http://eago.com.mx/q-subastas/images/banner970x90.png" alt="">
+
+                      </div>
+              </div>
+
+                    <div class="col-md-12 p-3">
+                        <div class="row">
+                             <div class="col-auto">
+                                <img class="rounded p-2" src="icons/auction.png" alt="" style="background-color: #05123F;width: 50px;height: 50px">
+                            </div>
+
+                             <div class="col-auto">
+                                <h3>Subastas</h3>
+                                <p>Subasta abierta</p>
+                            </div>
+
+                            <div class="col-auto p-3">
+                                <button class="btn btn-info boton-redondeado">Activa</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+              <div class="row">
+                <div class="col-md-12">
+ 
+
+                  <div id="card-561335">
+                    <div class="card">
+                    <a class="card-link collapsed text-white" data-toggle="collapse" data-parent="#card-561335" href="#card-element-687073">
+                        <div class="card-header" style="background-color: #05123F">
+                      
+                           <div class="d-flex align-content-start flex-wrap">+
+                          
+                               <div class="text-white col-md-auto">
+                                  <h6>Auto</h6>
+                                  <p>abarth</p>
+                              </div>
+                               <div class="text-white col-md-2">
+                                  <h6>Mejor Ofertador</h6>
+                                  <p>Alejandro</p>
+                              </div>
+                               <div class="text-white col-md-2">
+                                  <h6>Oferta + alta</h6>
+                                  <p>$96.00</p>
+                              </div>
+                               <div class="text-white col-md-2">
+                                  <h6>Oferta Actual</h6>
+                                  <p>$56,2500</p>
+                              </div>
+                               <div class="text-white col-md-2">
+                                  <h6>Estatus</h6>
+                                  <p>Activo</p>
+                              </div>
+                               <div class="text-white col-md-auto">
+                                  <h6>Tiempo Restante</h6>
+                                  <button class="btn btn-info btn-sm" type="">1  : 35 ; 20</button>
+                              </div>
+                            </div>
+                           
+                        </div>
+                      </a>
+
+                      <div id="card-element-687073" class="collapse">
+                        <div class="card-body">
+
+                          <div class="col-md-12 p-3">
+                              <div class="row">
+                                   <div class="col-md-2">
+                                    <img class="img-fluid" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="img-thumbnail" />
+                                  </div>
+
+                                   <div class="col-12 col-sm-6 col-md p-3">
+
+                                      <table class="table ">
+                                          <thead class="">
+                                            <tr>
+                                              <th scope="col">Modelo</th>
+                                              <th scope="col">Color</th>
+                                              <th scope="col">Kilometraje</th>
+                                              <th scope="col">Precio</th>
+                                            </tr>
+                                          </thead>
+                                          <tbody>
+                                            <tr>
+                                              <td>2014</td>
+                                              <td>rojo</td>
+                                              <td>100km</td>
+                                              <td>$1000</td>
+                                            </tr>
+                                          </tbody>
+                                      </table>
+                                      <p class="card-text">
+                                        Descipcion:
+                                        <input disabled="" class="form-control" type="text" placeholder="Readonly input here…" readonly>
+                                      </p>
+
+                                   <div class="col-md-12 p-3">
+                                        <div class="row text-center">
+                                             <div class="col-md-4">
+                                                <h5>Aumento</h5>
+                                                <button class="btn-info boton-redondeado  p-2" type="">11,200</button>
+                                            </div>
+
+                                             <div class="col-md-4">
+                                                <h5>Ofertas estantes</h5>
+                                                <button class="btn-info boton-redondeado p-2" type="">10</button>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <h5>Ofertar</h5>
+                                                <label class="sr-only" for="">Ferrari-124spider</label>
+                                                <div class="input-group mb-2 mr-sm-2 ">
+                                                  <div class="input-group-text bg-info input-izquerdo">$</div>
+
+                                                  <input type="text" class="form-control" id="" placeholder="Ferrari-124spider">
+
+                                                  <div class="input-group-prepend " data-toggle="modal" data-target="#exampleModal" style="cursor: pointer;">
+                                                    <div class="input-group-text bg-info " style="border-radius: 0  15px 15px 0;">Ofertar</div>
+                                                  </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                  </div>
+
+                              </div>
+
+                          </div>
+
+
+
+  
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+        </div>
+    </div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content" style="border-radius: 15px;">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Confirmacion</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body text-center">
+        Estas seguro de que quieres ofertar
+        <h2>$211,500</h2>
+      </div>
+      <div class="modal-footer" style="padding: 0px;">
+        
+          <button type="button" class="btn btn-danger"style="width: 100%;margin-right:0px;border-radius: 0px;padding: 15px;border-radius: 0 0 0 15px ;">Cancelar</button>
+          <button type="button" class="btn btn-info" style="width: 100%;margin-left:0px;border-radius: 0px;padding: 15px;border-radius: 0 0 15px 0;">Aceptar</button>  
+      </div>
+    </div>
+  </div>
+</div>
+    <div class="overlay"></div>
+
+<?php 
+
+    include("scripts.php");
+
+ ?>
+
+</body>
+
+</html>
